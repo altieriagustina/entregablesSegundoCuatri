00b96219a912a413cac7cae91965e92266f77929
altieriagustina <altieriagustina@gmail.com>
2025-03-22 13:37:43 -0300

    Primer entregable POO

---
+++ TP1/index.ts
@@ -0,0 +1,12 @@
+import { Auto } from "./auto";
+
+// Instanciar objetos y probar métodos.
+
+let autoUno = new Auto("Fiat", "Toro", 2016, 40, "Diesel", "Manual");
+let autoDos = new Auto("Toyota", "Yaris", 2023, 0, "Nafta", "Automatica");
+
+console.log(autoUno);
+
+autoDos.acelerar(50);
+
+autoUno.frenar();
